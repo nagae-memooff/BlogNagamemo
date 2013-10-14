ca60447f29539579702e3d227705a1d3c08c499e
nagae-memooff <169192@gmail.com>
2013-10-14 10:38:03 +0800

    reposity first initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogNagamemo::Application.config.secret_key_base = '7fc1c996d00df9b4f8d1c7d9d1b8c362603dee51062bd0458201898a51e5a4055eea010e6de2baf1603dd040babcc0c6da44fe5ca56d1ab3238405fd1dcb2f71'
