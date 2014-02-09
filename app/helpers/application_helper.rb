a98db8e0e3b038d2223ed254941dc1831e53df95
nagae-memooff <169192@gmail.com>
2014-02-09 19:51:32 +0800

    修正头像文件在生产模式中由于编译静态资源而无法被正确读取的bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 module ApplicationHelper
   def set_portrait(user, size='s')
-    portrait_path = if File.exist? "#{Rails.root}/app/assets/images/portraits/#{user.id}.png"
-                      "/assets/portraits/#{user.id}.png"
-                    else
-                      "/assets/portraits/default_portrait.png"
-                    end
+    if Rails.env == 'development'
+      portrait_path = if File.exist? "#{Rails.root}/app/assets/images/portraits/#{user.id}.png"
+                        "/assets/portraits/#{user.id}.png"
+                      else
+                        "/assets/portraits/default_portrait.png"
+                      end
+    elsif Rails.env == 'production'
+      portrait_path = if File.exist? "/home/nagae-memooff/rails/blog_nagamemo/shared/portraits/#{user.id}.png"
+                        "/home/nagae-memooff/rails/blog_nagamemo/shared/portraits/#{user.id}.png"
+                      else
+                        "/home/nagae-memooff/rails/blog_nagamemo/shared/portraits/default_portrait.png"
+                      end
+    end
     name = user.name
     print_html = "<img class='portrait_#{size}' src='#{portrait_path}' alt='#{name}'/>"
     print_html
