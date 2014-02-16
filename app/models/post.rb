93d870b22c4d43838cdfddc3affbd4620aa61133
nagae-memooff <169192@gmail.com>
2014-02-16 23:10:53 +0800

    初步添加文件上传相关模型

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base
+  validates :user_id, presence: true
+
   belongs_to :user
   has_many :comments
-  validates :user_id, presence: true
+  has_many :file_records
 
   default_scope { order('id desc') }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
 
 	has_many :posts
 	has_many :comments
+  has_many :file_records
 
 	def User.new_remember_token
 		SecureRandom.urlsafe_base64
